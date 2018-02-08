693b628957d0cf49d73580a9dc77eb41e886d654
parker lindley <fugazi8@gmail.com>
2018-02-07 19:37:29 -0700

    Set up server and setstate of inputs on change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,21 +4,63 @@ export default class Login extends Component {
   constructor(props){
     super(props);
     this.state = {
-      string: ''
+      username: '',
+      password: ''
     }
   }
 
-  componentDidMount() {
-    this.setState({ string: 'hello'})
+  handleChange=(e)=> {
+    this.setState({[e.target.name]:e.target.value})
+  }
+
+  handleInput = (e) => {
+    e.preventDefault();
+
+    //     fetch('/api/v1/groceries', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ grocery }),
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    // .then(response => response.json())
+    // .then(groceries => {
+    //   this.setState({
+    //     grocery: {
+    //       name: '',
+    //       quantity: '' 
+    //     }
+    //   }, updateGroceryList(groceries));
+    // })
+    // .catch(error => {
+    //   this.setState({
+    //     errorStatus: 'Error adding grocery'
+    //   })
+    // });
   }
 
   render() {
     return (
       <div>
         <form type="submit">
-          <input type='text' placeholder="User Name or E-mail" />
-          <input type='password' placeholder="Password" />
-          <button type="submit">login</button>
+          <input
+            name="username" 
+            type='text' 
+            placeholder="User Name or E-mail"
+            value={this.state.username}
+            onChange={this.handleChange}
+            />
+          <input 
+            name="password"
+            type='password' 
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange} 
+            />
+          <button 
+            onClick={this.handleInput}
+            type="submit">login
+          </button>
         </form>
       </div>
     )
