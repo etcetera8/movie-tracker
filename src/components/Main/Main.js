b719d01daf262d290860144c2f755f71b5775773
Bruce Chung <brucekchung@gmail.com>
2018-02-10 10:44:24 -0700

    Refactor MovieContainer and Main into SFC, add FavoriteContainer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,20 +4,16 @@ import { connect } from 'react-redux';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import Login from '../Login/Login';
 
-export class Main extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path = '/' 
-               component={MovieContainer} />
-        <Route path = '/login' 
-               render={() => (this.props.loginStatus ? 
-                  (<Redirect to='/'/>) 
-                  : (<Login />))} />
-      </Switch>
-    )
-  }
-}
+export const Main = ({loginStatus}) => (
+  <Switch>
+    <Route exact path = '/' 
+           component={MovieContainer} />
+    <Route path = '/login' 
+           render={() => (loginStatus ? 
+              (<Redirect to='/'/>) 
+              : (<Login />))} />
+  </Switch>
+)
 
 const mapStateToProps = (state) => ({
   loginStatus: state.activeUser
