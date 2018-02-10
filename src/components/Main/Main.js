318a90e27cb66c82d01f280d89eaaa07f21804ba
Ophus Wong <ophus.wong@gmail.com>
2018-02-10 15:06:10 -0700

    Add redirect in Main to Login

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -13,7 +13,7 @@ export const Main = ({loginStatus}) => (
     />
     <Route 
       path="/favorites"
-      component={FavoriteContainer}
+      render={() => (loginStatus ? <FavoriteContainer /> : <Login />)}
     />
     <Route 
       path="/login" 
