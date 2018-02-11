a3a9641d332075938042de3a40fa78749f570e7e
Bruce Chung <brucekchung@gmail.com>
2018-02-11 11:25:26 -0700

    Position card and header elements

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,13 +5,10 @@ const Card = ({ movie , id, handleFavorite, addClass }) => {
   const {title, vote_average, vote_count, overview, poster_path} = movie;
   return (
     <div id={id} className={`MovieCard ${addClass}`}>
-      <h1 className="movie-title">{title}</h1>
       <button 
         className="favorite-btn"
         onClick={() => handleFavorite(movie)}>FAVORITE</button>
       <img src={poster_path} />
-      <h4>Vote Average: {vote_average}</h4>
-      <h4>Votes :{vote_count}</h4>
     </div>
   )
 }
