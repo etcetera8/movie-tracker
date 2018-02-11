666d90e6989ff719ff21b25e1c4ad7dfa29dceb3
Ophus Wong <ophus.wong@gmail.com>
2018-02-11 11:12:54 -0700

    Add hover state css styling for Card Add perspective to MovieContainer for hover flip effect Add MovieCard back section with title, overview, vote avg, and vote count Add styling to Card css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,15 +4,22 @@ import './Card.css'
 const Card = ({ movie , id, handleFavorite, addClass }) => {
   const {title, vote_average, vote_count, overview, poster_path} = movie;
   return (
-    <div id={id} className={`MovieCard ${addClass}`}>
-      <h1 className="movie-title">{title}</h1>
-      <button 
-        className="favorite-btn"
-        onClick={() => handleFavorite(movie)}>FAVORITE</button>
-      <img src={poster_path} />
-      <h4>Vote Average: {vote_average}</h4>
-      <h4>Votes :{vote_count}</h4>
-    </div>
+    <article  id={id}
+      className={`MovieCard ${addClass}`}
+      ontouchstart="this.classList.toggle('hover')">
+        <div className="MovieCard-front">
+          <button
+            className="favorite-btn"
+            onClick={() => handleFavorite(movie)}>FAVORITE</button>
+          <img src={poster_path} />
+        </div>
+        <div className="MovieCard-back">
+          <h1 className="movie-title">{title}</h1>
+          <p className="movie-overview">{overview}</p>
+          <h4>Vote Average: {vote_average}</h4>
+          <h4>Votes :{vote_count}</h4>
+        </div>
+    </article>
   )
 }
 
